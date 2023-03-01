254675d933615c56047531907c3b2fa1d10a39c1
Julian Kocher <kocherju@gmail.com>
2023-02-28 21:11:37 -0500

    updated safenft test, gitignore

--- safenft/test/safenft.js
+++ safenft/test/safenft.js
@@ -16,23 +16,13 @@ describe("safeNFT contract", function () {
 	await hhAttackerContract.connect(attacker).attackStepOne({ value: ethers.utils.parseEther("0.01") })
 	
 	result = await hhBaseContract.canClaim(hhAttackerContract.address)
-	console.log("Attacker can claim? ", result)
+	expect(result).to.equal(true)
 	
 	await hhAttackerContract.connect(attacker).attackStepTwo()
 	result = await hhBaseContract.connect(attacker).balanceOf(attacker.address)
-	console.log("Attacker NFT Balance: ", result)
+	expect(result).to.equal("5")
 	
 	result = await hhBaseContract.canClaim(hhAttackerContract.address)
-	console.log("Attacker can claim? ", result)
-	
-	// await hardhatContract.connect(attacker).addToWhitelist(attacker.address);
-	// await hardhatContract.connect(attacker).changeOwner(attacker.address);
-	//
-	// let result = await hardhatContract.connect(attacker).isOwner()
-	// expect(result).to.equal(true)
-	//
-	// await hardhatContract.connect(attacker)["pwn(address)"](attacker.address);
-	// result = await hardhatContract.isHacked()
-	// expect(result).to.equal(true)
+	expect(result).to.equal(false)
   });
 });
